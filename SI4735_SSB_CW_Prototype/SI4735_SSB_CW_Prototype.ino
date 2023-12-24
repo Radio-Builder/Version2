8c3971d5f5211a5402a73ac0aa5d468a53e69750
irishswguy <irish_guy@hotmail.com>
2023-12-24 15:22:14 +0000

    Added Si4730_SSB_CW_Prototype

---
+++ SI4735_SSB_CW_Prototype/SI4735_SSB_CW_Prototype.ino
@@ -0,0 +1,191 @@
+#include <SI4735.h>
+#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
+#include <SPI.h>
+#include "AiEsp32RotaryEncoder.h"
+#include "si5351.h"
+#include "Wire.h"
+
+//------------------------------- TFT Display Init ------------------------------//
+TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
+void setupDisplay(void)
+{
+  tft.init();
+  tft.setRotation(3);
+  tft.fillScreen(TFT_WHITE);
+}
+
+//------------------------------- Encoder Init ------------------------------//
+#define ROTARY_ENCODER_A_PIN 34
+#define ROTARY_ENCODER_B_PIN 35
+#define ROTARY_ENCODER_BUTTON_PIN 32
+#define ROTARY_ENCODER_STEPS 4
+#define ROTARY_ENCODER_VCC_PIN -1
+AiEsp32RotaryEncoder rotaryEncoder = AiEsp32RotaryEncoder(ROTARY_ENCODER_A_PIN, ROTARY_ENCODER_B_PIN, ROTARY_ENCODER_BUTTON_PIN, ROTARY_ENCODER_VCC_PIN, ROTARY_ENCODER_STEPS);
+long lastEncoderReading = 0;
+
+void IRAM_ATTR readEncoderISR()
+{
+	rotaryEncoder.readEncoder_ISR();
+}
+
+void setupEncoder(void)
+{
+  	//we must initialize rotary encoder
+	rotaryEncoder.begin();
+	rotaryEncoder.setup(readEncoderISR);
+	rotaryEncoder.setAcceleration(0); //250); //or set the value - larger number = more accelearation; 0 or 1 means disabled acceleration
+}
+
+//------------------------------- Si4735 Init ------------------------------//
+#include <patch_init.h> // SSB patch for whole SSBRX initialization string
+
+const uint16_t size_content = sizeof ssb_patch_content; // see ssb_patch_content in patch_full.h or patch_init.h
+
+#define AM_FUNCTION 1
+#define RESET_PIN 13
+
+#define LSB 1
+#define USB 2
+
+bool disableAgc = true;
+bool avc_en = true;
+
+int currentBFO = 0;
+
+// Some variables to check the SI4735 status
+uint16_t currentFrequency=7031;
+uint16_t lastFrequency=7031;
+uint8_t currentStep = 1;
+uint8_t currentBFOStep = 100;
+
+uint8_t bandwidthIdx = 2;
+const char *bandwidth[] = {"1.2", "2.2", "3.0", "4.0", "0.5", "1.0"};
+
+
+long et1 = 0, et2 = 0;
+
+typedef struct
+{
+  uint16_t minimumFreq;
+  uint16_t maximumFreq;
+  uint16_t currentFreq;
+  uint16_t currentStep;
+  uint8_t currentSSB;
+} Band;
+
+Band band[] = {
+    {520, 2000, 810, 1, LSB},
+    {3500, 4000, 3700, 1, LSB},
+    {7000, 7200, 7031, 1, LSB},
+    {11700, 12000, 11940, 1, USB},
+    {14000, 14300, 14200, 1, USB},
+    {18000, 18300, 18100, 1, USB},
+    {21000, 21400, 21200, 1, USB},
+    {24890, 25000, 24940, 1, USB},
+    {27000, 27700, 27300, 1, USB},
+    {28000, 28500, 28400, 1, USB}};
+
+const int lastBand = (sizeof band / sizeof(Band)) - 1;
+// int currentFreqIdx = 9;
+int currentFreqIdx = 2;
+uint8_t currentAGCAtt = 0;
+
+uint8_t rssi = 0;
+
+
+SI4735 si4735;
+
+
+
+void loadSSB()
+{
+  si4735.setI2CFastModeCustom(500000); // Increase the transfer I2C speed
+  si4735.loadPatch(ssb_patch_content, size_content); // It is a legacy function. See loadCompressedPatch 
+  si4735.setI2CFastModeCustom(100000); // Set standard transfer I2C speed
+}
+
+
+
+void setup()
+{
+   Serial.begin(115200);
+   while(!Serial);
+
+  setupDisplay();
+  setupEncoder();
+
+// Gets and sets the Si47XX I2C bus address
+  int16_t si4735Addr = si4735.getDeviceI2CAddress(RESET_PIN);
+  if ( si4735Addr == 0 ) {
+    Serial.println("Si473X not found!");
+    Serial.flush();
+    while (1);
+  } else {
+    Serial.print("The Si473X I2C address is 0x");
+    Serial.println(si4735Addr, HEX);
+  }
+
+
+  si4735.setup(RESET_PIN, AM_FUNCTION);
+
+  delay(10);
+  Serial.println("SSB patch is loading...");
+  et1 = millis();
+  loadSSB();
+  et2 = millis();
+  Serial.print("SSB patch was loaded in: ");
+  Serial.print( (et2 - et1) );
+  Serial.println("ms");
+  delay(100);
+  si4735.setTuneFrequencyAntennaCapacitor(1); // Set antenna tuning capacitor for SW.
+  si4735.setSSB(band[currentFreqIdx].minimumFreq, band[currentFreqIdx].maximumFreq, band[currentFreqIdx].currentFreq, band[currentFreqIdx].currentStep, band[currentFreqIdx].currentSSB);
+  delay(100);
+  currentFrequency = si4735.getFrequency();
+  si4735.setAvcAmMaxGain(60); // Sets the maximum gain for automatic volume control on AM/SSB mode (from 12 to 90dB)
+  si4735.setVolume(60);
+  si4735.setAutomaticGainControl(1, currentAGCAtt);
+  si4735.setSSBAudioBandwidth(bandwidthIdx);
+  if (bandwidthIdx == 0 || bandwidthIdx == 4 || bandwidthIdx == 5)
+        si4735.setSSBSidebandCutoffFilter(0);
+  else
+        si4735.setSSBSidebandCutoffFilter(1);
+  
+  
+  tft.setTextColor(TFT_WHITE,TFT_BLACK );  
+  tft.drawString("Freq ",5,30,4);  
+  tft.setTextColor(TFT_BLACK,TFT_WHITE );  
+  String freq = String(float(currentFrequency)/1000.0,3)+ "  ";
+  tft.drawString(freq,70,30,4);  
+
+}
+
+// Main
+void loop()
+{ 
+  long currentReading = rotaryEncoder.readEncoder();
+  long change =  currentReading -lastEncoderReading;
+  bool update = false;  
+  static int lastState = 0;
+
+  if(change !=0){
+    if(change > 0 ) change =1;
+    if(change < 0 ) change =-1;
+
+    currentFrequency = currentFrequency + change;
+
+    lastEncoderReading = currentReading;
+    update = true;  
+    }
+
+
+  
+  if(update==true){
+    String freq = String(float(currentFrequency)/1000.0,3) + "   ";
+    tft.drawString(freq,70,30,4)+ "   ";
+//    si4735.setSSBBfo(currentBFO);
+    if(lastFrequency != currentFrequency)
+      si4735.setFrequency(currentFrequency);
+    lastFrequency = currentFrequency;
+  }
+    
+}
